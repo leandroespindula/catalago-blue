9130c8d7e27326787accd954b52626054b6840d2
Leandro <leandroepindula@gmail.com>
2022-05-04 22:31:05 -0300

    JavaScript interpretando o banco de dados PostGres

---
+++ src/model/carros.js
@@ -0,0 +1,33 @@
+import Sequelize from 'sequelize'
+import {connection} from '../database/connection.js'
+
+export const carros = connection.define('carros', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    modelo:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    marca: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    ano: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    img: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+
+}, {
+    freezeTableName: true,
+    createdAt: false, 
+    updatedAt: false,
+    timestamps: false
+})
