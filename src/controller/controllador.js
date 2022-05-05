6b51a70728d6b3d1343838805c41d5a7bae913d2
Leandro <leandroepindula@gmail.com>
2022-05-04 22:34:18 -0300

    Configurações iniciais

--- src/controller/controllador.js
+++ src/controller/controllador.js
@@ -1,4 +1,11 @@
+import { connection } from "../database/connection.js"
+import {carros} from '../model/carros.js'
 
-export const getIndex = (req, res) =>{
+
+export const getIndex = async (req, res) =>{
+    const teste = await connection.query('SELECT * FROM carros', {
+        model: carros
+    })
+    console.log(teste)
     res.render('index.ejs')
 }
